39b46869286b2ae462e2511bf1a92323954d8663
varenichek <vasandra22@gmail.com>
2025-03-01 00:23:19 +0200

    Added basic stuff
    Added main menu, simple scene for testing, basic functionality for fox (develop later)

---
+++ scripts/static/menu/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+#TODO 
+const TEMP_SCENE_FOR_TESTING: Resource = preload("res://scenes/static/temp_scene_for_testing.tscn")
+
+func _on_btn_new_game_pressed() -> void:
+	get_tree().change_scene_to_packed(TEMP_SCENE_FOR_TESTING)
+
+
+func _on_btn_about_authors_pressed() -> void:
+	pass #TODO create authors page dedicated only for us
+
+
+func _on_btn_quit_pressed() -> void:
+	get_tree().quit()
---
+++ scripts/static/temp_scene_for_testing.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+const ENTITY : Resource = preload("res://scenes/entities/fox.tscn")
+
+func _ready() -> void:
+	spawn(ENTITY)
+
+
+#func _process(delta: float) -> void:
+	#pass
+
+
+func spawn(player_prel) -> void:
+	var player_inst = player_prel.instantiate()
+	player_inst.position.y += 2
+	add_child(player_inst)
