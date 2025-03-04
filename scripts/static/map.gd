a399c6dfee4b9a93bf2d4aa895d336206ba7820f
varenichek <vasandra22@gmail.com>
2025-03-04 21:12:42 +0200

    фыв

---
+++ scripts/static/map.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+const FOX = preload("res://scenes/entities/fox.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn(FOX)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+
+func spawn(player_prel) -> void:
+	var player_inst = player_prel.instantiate()
+	player_inst.position.y += 2
+	add_child(player_inst)
