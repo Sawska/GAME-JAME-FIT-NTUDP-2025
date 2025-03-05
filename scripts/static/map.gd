862457cf9db9b4a6eb1123470b2acaae10c1b867
Danilo <danyakstk@gmail.com>
2025-03-05 17:54:16 +0200

    эээ ууЭ

--- scripts/static/map.gd
+++ scripts/static/map.gd
@@ -4,6 +4,7 @@ const FOX = preload("res://scenes/entities/fox.tscn")
 const CHICKEN_BOT = preload("res://scenes/entities/chicken_bot.tscn")
 const DOG_BOT = preload("res://scenes/entities/dog_bot.tscn")
 const FOX_HOLE: Resource = preload("res://scenes/static/fox_hole.tscn")
+const FOX_HOLE_NIGHT: Resource = preload("res://scenes/static/fox_hole_night.tscn")
 
 var HaveAlreadySeen: bool = false
 var HaveAlreadyFound: bool = false
